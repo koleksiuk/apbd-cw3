d801e9379f6b149bb1d8361c0fcf5e59fbad81d8
Konrad Oleksiuk <konole@gmail.com>
2020-04-05 15:48:09 +0200

    Zadanie 6

--- cw3/Controllers/StudentsController.cs
+++ cw3/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using cw3.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -31,6 +32,13 @@ namespace cw3.Controllers
             }
 
             return NotFound("Nie znaleziono");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
         }
     }
 }
