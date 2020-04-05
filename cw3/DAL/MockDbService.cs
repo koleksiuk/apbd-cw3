932c6e3e70b62af2cf8924e435f6671c2135fed2
Konrad Oleksiuk <konole@gmail.com>
2020-04-05 16:10:48 +0200

    Zadanie 8

---
+++ cw3/DAL/MockDbService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using cw3.Models;
+
+namespace cw3.DAL
+{
+    public class MockDbService : IDbService
+    {
+        private static readonly IEnumerable<Student> _students;
+
+        static MockDbService()
+        {
+            _students = new List<Student>
+            {
+                new Student{IdStudent=1, FirstName="Jan", LastName="Kowalski"},
+                new Student{IdStudent=2, FirstName="Anna", LastName="Kowalska"},
+                new Student{IdStudent=3, FirstName="Paweł", LastName="Nowak"}
+            };
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+
+    }
+}
